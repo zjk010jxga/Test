8a6671559261230bb1aa3189cb6b0fde91517d84
Tony <tony@gmail.com>
2019-04-29 13:48:55 +0800

    new sourcetree

---
+++ app/src/main/java/com/example/test/gson/Forecast.java
@@ -0,0 +1,23 @@
+package com.example.test.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Forecast {
+    public String date;
+    @SerializedName("tmp")
+    public Temperature temperature;
+
+    @SerializedName("cond")
+    public More more;
+
+    public class Temperature {
+        @SerializedName("max")
+        public String max;
+        @SerializedName("min")
+        public String min;
+    }
+    public class More{
+        @SerializedName("txt_d")
+        public String info;
+    }
+}
